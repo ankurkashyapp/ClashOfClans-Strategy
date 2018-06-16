1723b4fa5ece10c9d23faba0d7b15db4eeeb1e90
Ankur Kashyap <shiv.ankurkashyap@gmail.com>
2018-06-16 23:24:18 +0530

    Testing posgresql

--- src/main/java/com/strategycoc/StrategyController.java
+++ src/main/java/com/strategycoc/StrategyController.java
@@ -3,6 +3,7 @@ package com.strategycoc;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,7 @@ import com.strategycoc.service.TownhallService;
 
 
 @RestController
+@Scope("request")
 public class StrategyController {
 	
 	@Autowired
