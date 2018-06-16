11dfa155860f2a23abb5f65670329bb422bce782
Ankur Kashyap <shiv.ankurkashyap@gmail.com>
2018-06-16 22:52:29 +0530

    Testing posgresql

--- src/main/java/com/strategycoc/ClashOfClansStrategyApplication.java
+++ src/main/java/com/strategycoc/ClashOfClansStrategyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClashOfClansStrategyApplication {
+public class ClashOfClansStrategyApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClashOfClansStrategyApplication.class, args);
--- src/main/java/com/strategycoc/StrategyController.java
+++ src/main/java/com/strategycoc/StrategyController.java
@@ -1,16 +1,24 @@
 package com.strategycoc;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.strategycoc.persistance.repository.TownHallRepository;
 
+@Service
+@Transactional
 @RestController
 public class StrategyController {
 	
+	@Autowired
+	private TownHallRepository townHallRepository;
 	@RequestMapping("get_app")
 	public String getAppName() {
-		return "COC Strategy";
+		return townHallRepository.findAll().get(0).getTownhallName();
 	}
 
 }
