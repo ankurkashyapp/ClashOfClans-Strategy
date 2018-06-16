0037708e59b9aa50a27d123c0b0610613e9d0e70
Ankur Kashyap <shiv.ankurkashyap@gmail.com>
2018-06-16 16:43:13 +0530

    Renamed some files

--- src/main/java/com/strategycoc/ClashOfClansStrategyApplication.java
+++ src/main/java/com/strategycoc/ClashOfClansStrategyApplication.java
@@ -1,4 +1,4 @@
-package com.startegycoc;
+package com.strategycoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/strategycoc/StrategyController.java
+++ src/main/java/com/strategycoc/StrategyController.java
@@ -1,4 +1,4 @@
-package com.startegycoc;
+package com.strategycoc;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
