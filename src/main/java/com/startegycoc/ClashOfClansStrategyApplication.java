324c0c8381376ed5f7bdc817c28cd475271c524f
Ankur Kashyap <shiv.ankurkashyap@gmail.com>
2018-06-16 15:09:26 +0530

    Created the project

---
+++ src/main/java/com/startegycoc/ClashOfClansStrategyApplication.java
@@ -0,0 +1,12 @@
+package com.startegycoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClashOfClansStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClashOfClansStrategyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/startegycoc/ClashOfClansStrategyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.startegycoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClashOfClansStrategyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
